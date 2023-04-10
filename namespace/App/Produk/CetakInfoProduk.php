b0e93d13123c492003f3d6f6513022bbc336bd27
ahdianmirza <ahdianmirza@gmail.com>
2023-04-10 17:47:44 +0700

    menambahkan file latihan namespace

---
+++ namespace/App/Produk/CetakInfoProduk.php
@@ -0,0 +1,21 @@
+<?php
+class CetakInfoProduk
+{
+    public $daftarProduk = [];
+
+    public function tambahProduk(Produk $produk)
+    {
+        $this->daftarProduk[] = $produk;
+    }
+
+    public function cetak()
+    {
+        $str = "DAFAR PRODUK : <br>";
+
+        foreach ($this->daftarProduk as $p) {
+            $str .= "- {$p->getInfoProduk()} <br>";
+        }
+
+        return $str;
+    }
+}
